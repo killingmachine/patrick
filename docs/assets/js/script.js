3ed4d9b7dfc58f6535397b09c40c92108ac25033
killingmachine <thebackdoors182@gmail.com>
2017-06-19 12:36:08 +0800

    aa

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -34,13 +34,13 @@ var TxtType = function(el, toRotate, period) {
         if (this.isDeleting) { delta /= 2; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
-	        delta = this.period;
-	        this.isDeleting = true;
+            delta = this.period;
+            this.isDeleting = true;
         } 
         else if (this.isDeleting && this.txt === '') {
-	        this.isDeleting = false;
-	        this.loopNum++;
-	        delta = 500;
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
         }
 
         setTimeout(function() {
@@ -84,16 +84,16 @@ var TxtType = function(el, toRotate, period) {
         var skill = document.getElementsByClassName('svg');
         var elements = document.getElementsByClassName('typewrite');
         for (var i=0; i<elements.length; i++) {
-        	if(i == 1){
-        		continue;
-        	}
-        	else{
-        		var toRotate = elements[i].getAttribute('data-type');
-	            var period = elements[i].getAttribute('data-period');
-	            if (toRotate) {
-	              new TxtType(elements[i], JSON.parse(toRotate), period);
-	            }
-        	} 
+            if(i == 1){
+                continue;
+            }
+            else{
+                var toRotate = elements[i].getAttribute('data-type');
+                var period = elements[i].getAttribute('data-period');
+                if (toRotate) {
+                  new TxtType(elements[i], JSON.parse(toRotate), period);
+                }
+            } 
         }
         var css = document.createElement("style");
         css.type = "text/css";
